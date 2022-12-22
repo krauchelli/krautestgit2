7dc54720648bbc32a9c96ec08939daeace6c352b
krauchelli <mekaku26shi@gmail.com>
2022-12-22 10:50:08 +0700

    creating test python file

---
+++ sample1.py
@@ -0,0 +1,17 @@
+#while test again
+print("Welcome to the sample test of my second(?) repo git test")
+
+name = str(input("Please enter your name: "))
+num = int(input("Please enter desired looping sequences total: "))
+i = 0
+storage = []
+
+while num > i:
+    pos = int(input("enter pos: "))
+    storage.append(pos)
+    print(storage)
+    i += 1
+
+print()
+print("Thank you for visiting this test!, the result below showed up that this test is working!")
+print(storage)
